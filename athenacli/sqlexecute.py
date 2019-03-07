df5f57d07009be7e9c64ee0d234a32a04ab78ed5
Data Solution Cell 정성원 <swjung94@sk.com>
2019-03-07 13:19:07 +0900

    add async conn

--- athenacli/sqlexecute.py
+++ athenacli/sqlexecute.py
@@ -157,11 +157,11 @@ class SQLExecute(object):
         # cursor.description is not None for queries that return result sets,
         # e.g. SELECT or SHOW.
         try:
-			running = True
-			while running:
-    			running = future.running()
-    			click.echo(".", nl=False)
-    			time.sleep(1)
+            running = True
+            while running:
+                running = future.running()
+                click.echo(".", nl=False)
+                time.sleep(1)
             result_set = future.result()
             if result_set.description is not None:
                 headers = [x[0] for x in result_set.description]
